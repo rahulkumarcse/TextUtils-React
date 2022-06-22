7266f82e1d96d42a961fd5bfa4e4cc603ccb1efb
Rahul Kumar <rahulcsofficial@gmail.com>
2022-06-22 09:47:58 +0530

    first commit,dev started

---
+++ src/Aboutme.js
@@ -0,0 +1,10 @@
+function Aboutme(){
+    return(
+<p1>
+    I am Rahul , a FullStack Developer as well as Data Engineer  :) Having skills of both .
+</p1>
+    );
+}
+
+
+export default Aboutme;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Aboutme from './Aboutme';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Still In Development !!! <br/>
+        About me ? 
+        <a href='./Aboutme' onClick={<Aboutme/>}>Test Link</a>
+        
+      </header> 
+      <Aboutme/>
     </div>
+    
   );
 }
 
