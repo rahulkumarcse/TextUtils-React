cecc79ea6c9d05448d62234fe6db7c696e363317
Rahul Kumar <rahulcsofficial@gmail.com>
2022-07-05 22:50:15 +0530

    Alert added

---
+++ src/Componenets/Alert.js
@@ -0,0 +1,8 @@
+function Alert(props) {
+    return (
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    );
+}
