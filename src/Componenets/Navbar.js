5f1a7dcc20cd421c0e5456cb27d832e335c2dc10
Rahul Kumar <rahulcsofficial@gmail.com>
2022-07-07 21:05:34 +0530

    Dark mode applied for Navbar

--- src/Componenets/Navbar.js
+++ src/Componenets/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar(props) {
   return (
     <>
-      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <nav className={`navbar navbar-expand-lg navbar-dark bg-dark`}>
         <div className="container-fluid">
           <a className="navbar-brand" href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
