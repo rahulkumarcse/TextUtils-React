8a7d63bedbb78e2af8cc762cf35ae8c41c6fcbee
Rahul Kumar <rahulcsofficial@gmail.com>
2022-07-07 11:48:07 +0530

    WordCountHandler added

--- src/Componenets/Textform.js
+++ src/Componenets/Textform.js
@@ -9,37 +9,53 @@ export default function TextForm(props) {
     const clickUpHandler = (event) => {
         let temp = text.toUpperCase();
         setText(temp);
-        props.showAlert("Converted to uppercase","success");
+        props.showAlert("Converted to uppercase", "success");
     }
     const clickLcHandler = () => {
         setText(text.toLowerCase())
-        props.showAlert("Converted to Lowercase","success")
+        props.showAlert("Converted to Lowercase", "success")
     }
     const clearHandler = () => {
         setText("")
-        props.showAlert("Text cleared","success")
+        props.showAlert("Text cleared", "success")
     }
-    const removeExtraspaces =() => {
+    const removeExtraspaces = () => {
         let newText = text.split(/[ ]+/)
         setText(newText.join(" "))
-        props.showAlert("Extraspace has been removed","success")
+        props.showAlert("Extraspace has been removed", "success")
     }
     const handleCopy = () => {
         console.log("I am copy");
         var text = document.getElementById("inputTextForm");
         text.select();
         navigator.clipboard.writeText(text.value);
-        props.showAlert("Text copied to clipboard","success")
+        props.showAlert("Copied to clipboard", "success")
 
+    }
+
+    const wordCountHandler =() =>{
+        let varcount =0 ;
+        let splitArr = text.split(" ");
+        for (let i =0;i< splitArr.length;i++){
+          if(splitArr[i]!==''){ 
+            varcount++;
+          }
+        
+        }
+        console.log(varcount);
+
+       return varcount;
     }
     const [text, setText] = useState("");
     return (
         <>
-            <div className='container ' style={{color :props.mode==='dark'?'white':'black'}}>
+            <div className='container ' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" id="inputTextForm" rows="8" value={text} onChange={changeHandler} style={{backgroundColor :props.mode==='dark'?'grey':'white',
-                color: props.mode==='dark'?'white':'black'}}  placeholder="Enter your text here ..."></textarea>
+                    <textarea className="form-control" id="inputTextForm" rows="8" value={text} onChange={changeHandler} style={{
+                        backgroundColor: props.mode === 'dark' ? 'grey' : 'white',
+                        color: props.mode === 'dark' ? 'white' : 'black'
+                    }} placeholder="Enter your text here ..."></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={clickUpHandler}>Convert to uppercase</button>
                 <button className='btn btn-primary mx-2' onClick={clickLcHandler}>Convert to Lowercase</button>
@@ -49,11 +65,11 @@ export default function TextForm(props) {
 
 
             </div>
-            <div className='container my-2'style={{color :props.mode==='dark'?'white':'black'}}>
+            <div className='container my-2' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
-                <p>{text.split(/[ ]+/).length} Words and  {text.lenth} characters</p>
+                <p>{wordCountHandler()} Words and  {text.length} characters</p>
                 <h3>Preview</h3>
-                <p>{text.length >0 ? text : "Enter text above to preview "}</p>
+                <p>{wordCountHandler() > 0 ? text : "Enter text above to preview "}</p>
             </div>
         </>
     );
